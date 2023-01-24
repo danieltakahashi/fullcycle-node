1f80b36cae8171d86f539a85339333eec410213d
Daniel Takahashi <daniel.takahashi@objective.com.br>
2023-01-24 18:59:29 -0300

    Add random names and save its on mysql

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'fullcycle'
+}
+const connection = mysql.createConnection(config)
+var random_name = require('node-random-name');
+
+connection.query(`CREATE TABLE IF NOT EXISTS people(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY(id))`)
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+    connection.query(`INSERT INTO people(name) values('${random_name()}')`)
+    connection.query(`SELECT name FROM people`, function (err, results, fields) {
+        if (err) throw err
 
+        names = results.map((entry) => `- ${entry.name}`).join('<br>')
+
+        res.send('<h1>Full Cycle Rocks!</h1><br>' + names)
+    })
+
+    connection.end
 })
 
 app.listen(3000, () => {
